d3cfd979608c08e358547c5e849c1a848d1db5d2
Abror Alisherov <74832377+Abror2305@users.noreply.github.com>
2022-04-07 09:58:37 +0500

    home page completed

--- js/index.js
+++ js/index.js
@@ -3,26 +3,122 @@ const avatarImg = document.querySelector('.avatar-img')
 if(avatar){
     avatarImg.src = host+"/avatar/"+avatar
 }
+const members = document.querySelector("#members")
+const searchBox = document.querySelector(".search-box")
+const videosList = document.querySelector("#videosList")
+const datalist = document.querySelector("#datalist")
+const searchInput = document.querySelector(".search-input")
+const microphone = document.querySelector("#microphone")
 
-async function renderUser() {
-    let response = await requestJSON('/info', 'GET')
-    console.log(response);
-    if (response.users.length) {
-        users.innerHTML = `<h1>YouTube Members</h1>
-        <li class="channel active" data_id="main">
+window.addEventListener("DOMContentLoaded",async () => {
+    let users = await fetch(host + '/info')
+    users = await users.json()
+    renderUsers(users.users)
+    renderVideos()
+    searchBox.onsubmit = event => {
+        event.preventDefault()
+        searchInput.value.trim()
+        if(searchInput.value !== "") {
+            return renderVideos(false, searchInput.value)
+        }
+    }
+    const voice = new webkitSpeechRecognition()
+
+    voice.lang = 'uz-UZ'
+    voice.continious = false
+
+    voice.onresult = event => {
+        searchInput.value = event.results[0][0].transcript
+        return renderVideos(false, searchInput.value)
+    }
+
+    microphone.onclick = () => {
+        voice.start()
+    }
+    voice.onaudioend = () => {
+        voice.stop()
+    }
+    avatarImg.onclick = () => {
+        if(!window.localStorage.token){
+            window.location = "./register.html"
+        }
+        window.location = "./admin.html"
+    }
+})
+
+function renderUsers(users) {
+    console.log(users);
+    if (users.length) {
+        members.innerHTML = `<h1>YouTube Members</h1>
+        <li class="channel active" onclick="main(this)">
         <a href="#">
         <svg viewbox="0 0 24 24" focusable="false" style="pointer-events: none; display: block; width: 30px; height: 30px;"><g><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8" class="style-scope yt-icon"></path></g></svg>
         <span>Home</span>
         </a>
         </li>`
-        for (let user of response.users) {
-            users.innerHTML += `<li class="channel" userId=${user.userId}>
-            <a href="#">
-                <img src="${backendApi+'/'+user.img}" alt="channel-icon" width="30px" height="30px">
-                <span>${user.username}</span>
-            </a>
-        </li>`
+        for (let user of users) {
+            const [li,a,img,span] = createElements("li","a","img","span")
+            li.classList.add("chanel")
+            a.href = "#"
+            img.setAttribute("src",`${host+'/avatar/'+user.avatar}`)
+            img.alt = "chanel-icon"
+            img.style.width = "30px"
+            img.style.height = "30px"
+            span.textContent = user.username
+
+            a.append(img,span)
+            li.append(a)
+            members.append(li)
+
+            li.onclick = async () => {
+                const active = document.querySelector(".active")
+                active.classList.remove("active")
+                li.classList.add("active")
+                window.localStorage.currentUser = user.userId
+                renderVideos(user.userId)
+            }
         }
     }
 }
-console.log("hello")
+
+function main(e){
+    const active = document.querySelector(".active")
+    active.classList.remove("active")
+    e.classList.add("active")
+    renderVideos()
+}
+async function renderVideos(currentUser,search) {
+    videosList.innerHTML = ""
+    datalist.innerHTML = ""
+    let videos = await fetch(host + `/search?`+(currentUser?"userId="+currentUser:"")+ (search?"search="+search: ""))
+    videos = (await videos.json()).videos
+    for (let video of videos) {
+        if(!search) {
+            let [option] = createElements("option")
+            option.value = video.Title
+            datalist.append(option)
+        }
+        videosList.innerHTML += `
+        <li class="iframe">
+            <video src=${host + "/videos/" + video.fileName} controls=""></video>
+            <div class="iframe-footer">
+                <img src="${host + "/avatar/" + video.user.avatar}" alt="channel-icon">
+                <div class="iframe-footer-text">
+                    <h2 class="channel-name">${video.user.username}</h2>
+                    <h3 class="iframe-title">${video.Title}</h3>
+                    <time class="uploaded-time">${video.Created}</time>
+                    <a class="download" href="${host + "/video/" + video.fileName}" download>
+                        <span>${video.size} MB</span>
+                        <img alt="download" src="./img/download.png">
+                    </a>
+                </div>                  
+            </div>
+        </li>
+        `
+    }
+}
+
+
+// function
+
+// console.log("hello")
--- js/util.js
+++ js/util.js
@@ -1,1 +1,5 @@
-const host = "http://localhost:5000"
+const host = "http://192.168.1.7:5000"
+
+function createElements(...elements) {
+    return elements.map(el => document.createElement(el))
+}
